62c690232442543667c2add431d5cabdc2814bf5
moses35 <energetic143@gmail.com>
2023-08-20 15:13:42 +0300

    hw05-avatars

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,15 @@
 const { User } = require("../models/user");
 const { HttpError, ctrlWrapper } = require("../helpers");
+const path = require("path");
+const fs = require("fs/promises");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const gravatar = require("gravatar");
+const jimp = require("jimp");
 
 const { SECRET_KEY } = process.env;
+
+const avatarsDir = path.join(__dirname, "../", "public", "avatars");
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -15,8 +21,13 @@ const register = async (req, res) => {
   }
 
   const hachPassword = await bcrypt.hash(password, 10);
+  const avatarURL = gravatar.url(email);
 
-  const newUser = await User.create({ ...req.body, password: hachPassword });
+  const newUser = await User.create({
+    ...req.body,
+    password: hachPassword,
+    avatarURL,
+  });
 
   res.status(201).json({
     user: {
@@ -81,7 +92,30 @@ const updeteSubscription = async (req, res) => {
   if (!result) {
     throw HttpError(404, "Not found");
   }
-  res.json(result);
+  res.json({ subscription: result.subscription });
+};
+
+const updateAvatar = async (req, res) => {
+  const { _id } = req.user;
+  if (!req.file) {
+    throw HttpError(400, "missing file");
+  }
+
+  const { path: tempUpload, originalname } = req.file;
+
+  const image = await jimp.read(tempUpload);
+  image.resize(250, 250);
+  await image.writeAsync(tempUpload);
+
+  const filename = `${_id}_${originalname}`;
+  const resultUpload = path.join(avatarsDir, filename);
+  await fs.rename(tempUpload, resultUpload);
+  const avatarURL = path.join("avatars", filename);
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  res.json({
+    avatarURL,
+  });
 };
 
 module.exports = {
@@ -90,4 +124,5 @@ module.exports = {
   logout: ctrlWrapper(logout),
   getCurrent: ctrlWrapper(getCurrent),
   updeteSubscription: ctrlWrapper(updeteSubscription),
+  updateAvatar: ctrlWrapper(updateAvatar),
 };
